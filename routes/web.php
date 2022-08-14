035e429fa721b8f99703c12da04c58f4ab8bb80b
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-14 01:48:46 -0700

    testing 2

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            ]
+        ]
+    ]);
 });
 
-Route::get('posts/{id}', function($id) {
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
+// Route::get('posts/{id}', function($id) {
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
 
-Route::get('/search', function(Request $request) {
-    return $request->name. ' '. $request->city. ' ' . $request->age;
-});
+// Route::get('/search', function(Request $request) {
+//     return $request->name. ' '. $request->city. ' ' . $request->age;
+// });
