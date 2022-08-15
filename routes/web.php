4d1ab0bc1f42eb627ae75273689aa512b6109765
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-14 20:06:06 -0700

    Resource Method Naming

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,16 @@ Route::get('/', [ListingController::class, 'index']);
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
+
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing
+
 // Route::get('posts/{id}', function($id) {
 //     return response('Post ' . $id);
 // })->where('id', '[0-9]+');
