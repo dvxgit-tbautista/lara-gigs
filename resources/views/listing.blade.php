d6396b1a985f92f658d0cec568911472e8e58d77
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-14 18:40:00 -0700

    Added the Theme HTML

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,3 +1,9 @@
-    <h2>{{ $listing['title'] }}</h2>
-    <p>{{ $listing['description'] }}</p>
+@extends('layout')
 
+@section('content')
+
+
+<h2>{{ $listing['title'] }}</h2>
+<p>{{ $listing['description'] }}</p>
+
+@endsection
