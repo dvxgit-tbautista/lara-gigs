4be69cffc373057f658173f8f34dfe2aa3dfa955
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-15 01:15:28 -0700

    User Registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,6 +46,12 @@ Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
 // Delete Listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create new Users
+Route::post('/users', [UserController::class, 'store']);
 
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
