a98416e530e578836b32f368538be18a5b984897
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-14 21:10:33 -0700

    Validation and Store Listing

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -6,7 +6,8 @@
             </h2>
             <p class="mb-4">Post a gig to find a developer</p>
         </header>
-        <form action="">
+        <form method="POST" action="/listings">
+            @csrf
             <div class="mb-6">
                 <label
                     for="company"
@@ -18,6 +19,10 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="company"
                 />
+
+                @error('company')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
@@ -30,6 +35,9 @@
                     name="title"
                     placeholder="Example: Senior Laravel Developer"
                 />
+                @error('title')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
@@ -44,6 +52,10 @@
                     name="location"
                     placeholder="Example: Remote, Boston MA, etc"
                 />
+
+                @error('location')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
@@ -55,6 +67,10 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="email"
                 />
+
+                @error('email')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
@@ -69,6 +85,10 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="website"
                 />
+
+                @error('website')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
@@ -81,9 +101,13 @@
                     name="tags"
                     placeholder="Example: Laravel, Backend, Postgres, etc"
                 />
+
+                @error('tags')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
-            <div class="mb-6">
+            {{-- <div class="mb-6">
                 <label for="logo" class="inline-block text-lg mb-2">
                     Company Logo
                 </label>
@@ -92,7 +116,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="logo"
                 />
-            </div>
+            </div> --}}
 
             <div class="mb-6">
                 <label
@@ -107,6 +131,10 @@
                     rows="10"
                     placeholder="Include tasks, requirements, salary, etc"
                 ></textarea>
+
+                @error('description')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
